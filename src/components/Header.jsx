a7d7cc9a941ffd34fb8ba122a67d82292b713cce
yhonder710 <agueropibe1@gmail.com>
2025-03-22 12:18:00 -0400

    hearder 1.3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,12 +10,12 @@ import { IoShareSocial } from "react-icons/io5";
 export function Header () {
   return (
     <header className="w-full flex justify-center fixed z-10 top-5">
-      <nav className="h-12 w-full mx-5 px-3 sm:w-130 bg-black/50 rounded-4xl border border-white/40 text-white font-bold text-[13px] sm:text-[18px]">
+      <nav className="h-12 w-full mx-5 px-3 sm:w-130 bg-black/50 rounded-4xl border border-white/40 text-white font-bold text-[18px]">
       <ul className="h-full flex items-center justify-evenly">
-        <li><a href="#" className="flex items-center gap-1"><FaRegUser size={22}/>Sobre mi</a></li>
-        <li><a href="#" className="flex items-center gap-1"><IoShareSocial/> Social</a></li>
-        <li><a href="#" className="flex items-center gap-1"><BiBody size={25}/> habilidades</a></li>
-        <li><a href="#" className="flex items-center gap-1"><PiProjectorScreenChart size={25}/> Projectos</a></li>
+        <li><a href="#" className="flex items-center gap-1"><FaRegUser size={20}/><strong className="sm:inline hidden">Sobre mi</strong></a></li>
+        <li><a href="#" className="flex items-center gap-1"><IoShareSocial/> <strong className="sm:inline hidden">Social</strong></a></li>
+        <li><a href="#" className="flex items-center gap-1"><BiBody size={25}/> <strong className="sm:inline hidden">habilidades</strong></a></li>
+        <li><a href="#" className="flex items-center gap-1"><PiProjectorScreenChart size={25}/> <strong className="sm:inline hidden">Projectos</strong></a></li>
       </ul>
       </nav>
     </header>
