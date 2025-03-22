ee88bc8e1a8ca869a7bf8266c166d265b8e810e1
yhonder710 <agueropibe1@gmail.com>
2025-03-22 12:34:04 -0400

    presentacion 1.2

--- src/components/presentacion.jsx
+++ src/components/presentacion.jsx
@@ -3,8 +3,8 @@ export function Presentacion () {
     <>
      <div className="w-full flex flex-col md:flex-row justify-around items-center gap-10">
       <div className="w-full sm:160 flex flex-col gap-5">
-        <h1 className="text-5xl font-bold">Hola es un placer, soy Kpito</h1>
-        <p className="text-[18px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, deleniti porro! Possimus ratione error, ad deserunt, consequatur perspiciatis molestiae veniam ab laudantium delectus rerum corrupti minus blanditiis quisquam vel libero.</p>
+        <h1 className="text-3xl sm:text-5xl font-bold">Hola es un placer, soy Kpito</h1>
+        <p className="text-[13px] sm:text-[18px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, deleniti porro! Possimus ratione error, ad deserunt, consequatur perspiciatis molestiae veniam ab laudantium delectus rerum corrupti minus blanditiis quisquam vel libero.</p>
       </div>
       <picture><img className="w-50 h-50 rounded-full" src="https://img.freepik.com/vector-premium/cute-programador-depuracion-codigo-vector-dibujos-animados_865091-13405.jpg" alt="foto de perfil" /></picture>
     </div>
